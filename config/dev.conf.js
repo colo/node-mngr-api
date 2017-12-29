63810e64d2ebbb2d303d629b15b6c6a02e71b61d
Luciano Bueno <glamero@gmail.com>
2017-12-29 18:09:21 -0300

    bump

--- config/dev.conf.js
+++ config/dev.conf.js
@@ -4,11 +4,24 @@ const Moo = require("mootools"),
 		BaseApp = require ('./base.conf');
 
 //var winston = require('winston');
+const limit = require('node-limit/rate/request');
+var req_limit = new limit({
+									limit: 1,
+									interval: 500
+								});
 
+var req_max = new limit({
+									limit: 2,
+									interval: 5000
+								});
+								
 module.exports = new Class({
   Extends: BaseApp,
   
   options: {
+		
+		//middlewares: [req_limit.ip(), req_limit.user()],
+		middlewares: [req_limit.ip(), req_max.ip()],
 		
 		authentication: {
 			users : [
